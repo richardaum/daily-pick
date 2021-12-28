982544f6a42250ae6bf580e20f682989728bbc74
Richard Lopes <richard.lopes92@gmail.com>
2021-12-28 18:29:10 -0300

    test: add fixtures

---
+++ src/fixtures/remove.ts
@@ -0,0 +1,7 @@
+export const remove = {
+  query: {},
+  body: {
+    payload:
+      '{"type":"block_actions","user":{"id":"U02PXB0JD41","username":"richard.lopes92","name":"richard.lopes92","team_id":"T02PFPNF6BF"},"api_app_id":"A02Q94LGHUG","token":"VBhCLtzdGkMlo6M8cz0DUumt","container":{"type":"message","message_ts":"1640724432.000800","channel_id":"C02RQ45BBV5","is_ephemeral":true},"trigger_id":"2874917373591.2797804516389.2bd024800e18d92db44252ee15353983","team":{"id":"T02PFPNF6BF","domain":"daily-pick"},"enterprise":null,"is_enterprise_install":false,"channel":{"id":"C02RQ45BBV5","name":"meutime"},"state":{"values":{}},"response_url":"https:\\/\\/hooks.slack.com\\/actions\\/T02PFPNF6BF\\/2882854138230\\/7fNuEHz7VdIvuTzhcU7v8VxF","actions":[{"action_id":"remove","block_id":"TFDdi","text":{"type":"plain_text","text":"Remover","emoji":true},"value":"mZ13W2vPMYao1tbQWiya","style":"danger","type":"button","action_ts":"1640724465.443721"}]}',
+  },
+};
