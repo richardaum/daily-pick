278b65317a11701d657647eca835a56f5c7ef45b
Richard Lopes <richard.lopes92@gmail.com>
2021-12-29 20:15:57 -0300

    chore: add google cloud loggin bunyan

--- src/services/env/index.ts
+++ src/services/env/index.ts
@@ -2,11 +2,14 @@ const config = {
   PORT: process.env.PORT || '3333',
   FIREBASE_CONFIG: process.env.FIREBASE_CONFIG,
   LOG_DISABLED: process.env.LOG_DISABLED,
+  GOOGLE_CLOUD_LOGGING_BUNYAN_ENABLED: process.env.GOOGLE_CLOUD_LOGGING_BUNYAN_ENABLED,
 };
+
+const required = new Set(['PORT', 'FIREBASE_CONFIG']);
 
 export const env = (key: keyof typeof config) => {
   const value = config[key] ?? process.env[key];
-  if (value == null) throw new Error(`Missing required environment variable ${key}`);
+  if (value == null && required.has(key)) throw new Error(`Missing required environment variable ${key}`);
   return value;
 };
 
--- src/services/logger/index.ts
+++ src/services/logger/index.ts
@@ -1,3 +1,4 @@
+import { LoggingBunyan } from '@google-cloud/logging-bunyan';
 import bunyan from 'bunyan';
 
 import { env } from '@/services/env';
@@ -6,7 +7,10 @@ export function createLogger() {
   const logger = bunyan.createLogger({
     name: 'daily-pick',
     src: true,
-    streams: [{ stream: process.stdout, level: 'info' }],
+    streams: [
+      { stream: process.stdout, level: 'info' },
+      ...(env('GOOGLE_CLOUD_LOGGING_BUNYAN_ENABLED') === 'true' ? [new LoggingBunyan().stream('info')] : []),
+    ],
   });
 
   if (env('LOG_DISABLED') === 'true') {
