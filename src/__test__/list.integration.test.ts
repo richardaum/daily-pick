b8191331d6581b6ae13d8457d769bd2fa1d3f5be
Richard Lopes <richard.lopes92@gmail.com>
2021-12-29 18:49:40 -0300

    test: list

---
+++ src/__test__/list.integration.test.ts
@@ -0,0 +1,18 @@
+import { request } from './server';
+
+import { list } from '@/fixtures/list';
+import * as functions from '@/services/database/functions/fetchCronsByChannelAndTeam';
+
+jest.mock('@/bootstrap/schedule');
+
+describe('createCron', () => {
+  it('should display an empty list', async () => {
+    jest.spyOn(functions, 'fetchCronsByChannelAndTeam').mockResolvedValue([]);
+
+    const req = request().post('/api/commands/daily/pick');
+    const res = await req.send(list.body);
+
+    expect(JSON.stringify(res.body, null, 2)).toContain('Nenhum agendamento encontrado');
+    expect(res.statusCode).toBe(200);
+  });
+});
