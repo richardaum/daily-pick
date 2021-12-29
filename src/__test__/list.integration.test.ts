5fbffb415bed8f0d3d8737fc7fc4ddde2f67f689
Richard Lopes <richard.lopes92@gmail.com>
2021-12-29 19:08:44 -0300

    test: list with elements

--- src/__test__/list.integration.test.ts
+++ src/__test__/list.integration.test.ts
@@ -15,4 +15,36 @@ describe('listCrons', () => {
     expect(JSON.stringify(res.body, null, 2)).toContain('Nenhum agendamento encontrado');
     expect(res.statusCode).toBe(200);
   });
+
+  it('should display a list', async () => {
+    jest.spyOn(functions, 'fetchCronsByChannelAndTeam').mockResolvedValue([
+      {
+        id: '1',
+        intervals: ['* * * * *'],
+        channel: 'C1',
+        team: 'T1',
+        current: 'U1',
+        users: [],
+        createdAt: 'createdAt #1',
+      },
+      {
+        id: '2',
+        intervals: ['* * * * *'],
+        channel: 'C1',
+        team: 'T1',
+        current: 'U2',
+        users: [],
+        createdAt: 'createdAt #2',
+      },
+    ]);
+
+    const req = request().post('/api/commands/daily/pick');
+    const res = await req.send(list.body);
+    const body = JSON.stringify(res.body, null, 2);
+
+    expect(body).not.toContain('Nenhum agendamento encontrado');
+    expect(body).toContain('*createdAt #1*');
+    expect(body).toContain('*createdAt #2*');
+    expect(res.statusCode).toBe(200);
+  });
 });
