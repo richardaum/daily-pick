632021d1bcbcd01d53ecf846c2643d689f686c0b
Richard Lopes <richard.lopes92@gmail.com>
2022-11-02 15:14:48 -0300

    style: run prettier

--- src/services/express/index.ts
+++ src/services/express/index.ts
@@ -2,12 +2,9 @@ import express from 'express';
 
 import { setupSentry } from '../error-tracking';
 
-
 export const api = express();
 
 api.use(express.json());
 api.use(express.urlencoded({ extended: true }));
-
-
 
 setupSentry(api);
