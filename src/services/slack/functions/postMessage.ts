969135c825f4f6982814c3865d2d9344fde91e6d
Richard Lopes <richard.lopes92@gmail.com>
2022-11-14 08:44:04 -0300

    feat: change next time message

--- src/services/slack/functions/postMessage.ts
+++ src/services/slack/functions/postMessage.ts
@@ -3,7 +3,7 @@ import { Blocks, Elements, Surfaces } from 'slack-block-builder';
 import { slack } from '..';
 
 import { SKIP_ACTION } from '@/constants';
-import { ITS_YOU_TOMORROW, ITS_YOUR_TURN, SKIP } from '@/i18n';
+import { ITS_YOUR_TURN, NEXT_TIME, SKIP } from '@/i18n';
 
 type PostMessageNamedParams = { cronId: string; channel: string; current: string; next: string };
 
@@ -18,7 +18,7 @@ export const postMessage = async ({ cronId, channel, current, next }: PostMessag
         Blocks.Section({
           text: [
             `:tada: ${current}, ${ITS_YOUR_TURN} :calendar:`,
-            `:black_right_pointing_double_triangle_with_vertical_bar: ${ITS_YOU_TOMORROW}, ${next}. :tada:`,
+            `:black_right_pointing_double_triangle_with_vertical_bar: ${NEXT_TIME}, ${next}. :tada:`,
           ].join('\n'),
         }),
         Blocks.Actions().elements(Elements.Button({ text: SKIP, actionId: SKIP_ACTION, value: cronId }))
