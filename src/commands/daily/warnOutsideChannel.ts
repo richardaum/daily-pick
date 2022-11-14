0b10c5c80a63a8e9735951e20889fa5f4f133013
Richard Lopes <richard.lopes92@gmail.com>
2022-11-14 08:47:39 -0300

    feat: add OK button to outside channel warning

--- src/commands/daily/warnOutsideChannel.ts
+++ src/commands/daily/warnOutsideChannel.ts
@@ -1,11 +1,20 @@
 import { RespondFn, SlashCommand } from '@slack/bolt';
+import { Blocks, Elements, Surfaces } from 'slack-block-builder';
 
-import { OUTSIDE_CHANNEL_MESSAGE } from '@/i18n';
+import { DELETE_MESSAGE_ACTION } from '@/constants';
+import { DELETE_MESSAGE, OUTSIDE_CHANNEL_MESSAGE } from '@/i18n';
 
 export function isOutsideChannel(body: SlashCommand) {
   return body.channel_name === 'directmessage' || body.channel_name.startsWith('mpdm');
 }
 
 export async function warnOutsideChannel(respond: RespondFn) {
-  await respond(OUTSIDE_CHANNEL_MESSAGE);
+  await respond(
+    Surfaces.Message()
+      .blocks(
+        Blocks.Section({ text: OUTSIDE_CHANNEL_MESSAGE }),
+        Blocks.Actions().elements(Elements.Button({ text: DELETE_MESSAGE, actionId: DELETE_MESSAGE_ACTION }))
+      )
+      .buildToObject()
+  );
 }
