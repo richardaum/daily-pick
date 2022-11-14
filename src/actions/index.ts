e3d8fb260cc27a5118f70fe66ad711ba973fe058
Richard Lopes <richard.lopes@ifood.com.br>
2022-11-14 15:25:45 -0300

    feat: listen to app_home_opened event

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,6 +2,7 @@
 export * from './backToList';
 export * from './createCron';
 export * from './deleteMessage';
+export * from './home';
 export * from './listOverflow';
 export * from './openModal';
 export * from './skip';
