126cdd520317e262315d2cf1f4e776ad7d1ec447
Richard Lopes <richard.lopes92@gmail.com>
2022-11-14 08:44:04 -0300

    fix: specify message type

--- src/services/slack/functions/postMessage.ts
+++ src/services/slack/functions/postMessage.ts
@@ -9,6 +9,7 @@ type PostMessageNamedParams = { channel: string; responseUrl?: string; current: 
 export const postMessage = async ({ channel, responseUrl, current, next }: PostMessageNamedParams) => {
   if (responseUrl) {
     return await axios.post(responseUrl, {
+      response_type: 'in_channel',
       text: [
         `:tada: ${current}, ${ITS_YOUR_TURN} :calendar:`,
         `:black_right_pointing_double_triangle_with_vertical_bar: ${ITS_YOU_TOMORROW}, ${next}. :tada:`,
